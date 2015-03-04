1d92759abf9e669f29fd6c99fa128a4726abc277
unknown <fernando.arias@BUEMRM-DWS0016.MRM.local>
2015-03-04 11:28:52 -0300

    Encontrada la solución a JSONP responses issue.

--- Experimentation/JSONP/getJson.js
+++ Experimentation/JSONP/getJson.js
@@ -11,5 +11,7 @@ function json_process(data){
 }  
 function test(){  
     var url = "http://www.flickr.com/services/rest/?method=flickr.test.echo&format=json&api_key=fb3db427da4bcda80f74ea31c64cd64d&jsoncallback=json_process";  
+    // Probar también con este servicio: 
+    //https://www.gmfleet.com/bypass/gmna/dealerlocator/services/getdealers?type=PostalCode&format=JSON&x-country=US&x-language=en&x-brand=Chevrolet&postalcode=48048&callback=json_process
     jsonp(url);  
 }  
