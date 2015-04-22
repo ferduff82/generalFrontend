a303f4f199566c3d8188882e17e0fef507461a52
unknown <fernando.arias@BUEMRM-DW70015.la.corp.ipgnetwork.com>
2015-04-22 15:21:10 -0300

    First Angular Project.

---
+++ Experimentation/First Angular - Que auto quiero/app.js
@@ -0,0 +1,55 @@
+var app = angular.module('app', ['ngRoute'])
+
+  .factory('Todos', function(){
+    return [
+      { name: 'AngularJS Directives', completed: true, note: 'add notes...' },
+      { name: 'Data binding', completed: true, note: 'add notes...' },
+      { name: '$scope', completed: true, note: 'add notes...' },
+      { name: 'Controllers and Modules', completed: true, note: 'add notes...' },
+      { name: 'Templates and routes', completed: true, note: 'add notes...' },
+      { name: 'Filters and Services', completed: false, note: 'add notes...' },
+      { name: 'Get started with Node/ExpressJS', completed: false, note: 'add notes...' },
+      { name: 'Setup MongoDB database', completed: false, note: 'add notes...' },
+      { name: 'Be awesome!', completed: false, note: 'add notes...' },
+    ];
+  })
+
+  .controller('TodoController', ['$scope', 'Todos', function ($scope, Todos) {
+    $scope.todos = Todos;
+    $scope.name = 'World';
+    $scope.keyCode = "";
+    $scope.keyPressed = function(e) {
+      $scope.keyCode = e.which;
+    };
+  }])
+  
+  .controller('TodoDetailCtrl', ['$scope', '$routeParams', 'Todos', function ($scope, $routeParams, Todos) {
+    $scope.todo = Todos[$routeParams.id];
+  }])
+
+  app.directive('shortcut', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: true,
+      link: function postLink(scope, iElement, iAttrs){
+         $(document).on('keypress', function(e){
+           scope.$apply(scope.keyPressed(e));
+         });
+      }
+    };
+  })
+  
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/todos.html',
+        controller: 'TodoController'
+      })
+    
+      .when('/:id', {
+        templateUrl: '/todoDetails.html',
+        controller: 'TodoDetailCtrl'
+      });
+  }]);
+
