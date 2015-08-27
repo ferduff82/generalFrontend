96d220d701393c5f17516a1732d8418449c26e8f
Fernando Arias <ferduff82@hotmail.com>
2015-08-27 11:42:57 -0300

    Browser detection.

---
+++ Experimentation/Javascript Básico/browserDetection.js
@@ -0,0 +1,3 @@
+var browserChrome = /chrome/.test(navigator.userAgent.toLowerCase());
+var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+var is_IE = navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0;
