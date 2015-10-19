798cfb063315b410694e5a5d8793e226bf7a60fa
Fernando Arias <ferduff82@hotmail.com>
2015-10-19 15:53:55 -0300

    Job bugs.

--- Experimentation/Javascript Básico/cut URL with split, slice and join.js
+++ Experimentation/Javascript Básico/cut URL with split, slice and join.js
@@ -3,3 +3,12 @@ var getUrl = window.location.href,
     urlLanguage = urlCut.split('/').slice(1,5).join('/'),
     slashUrl = '/' + urlLanguage;
 console.log(slashUrl);
+
+
+//Improoved
+
+var getUrl = window.location.href,
+    urlCut = '/content' + getUrl.split('/content').slice(1),
+    urlLanguage = urlCut.split('/').slice(1,5).join('/'),
+    slashUrl = '/' + urlLanguage,
+    urlResponse = 'http:/' + getUrl.split('/').slice(1,3).join('/');
