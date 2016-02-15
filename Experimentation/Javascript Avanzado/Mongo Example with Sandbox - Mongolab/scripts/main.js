a4785bc54f76d09fdf51d9221a4efd59a651106f
Fernando Arias <ferduff82@hotmail.com>
2016-02-15 18:05:12 -0300

    Added Mongo test project.

---
+++ Experimentation/Javascript Avanzado/Mongo Example with Sandbox - Mongolab/scripts/main.js
@@ -0,0 +1,10 @@
+// Filename: main.js
+
+// Require.js allows us to configure shortcut alias
+// There usage will become more apparent further along in the tutorial.
+require.config({
+  paths: {
+    mongoose: '../node_modules/mongoose/lib/index',
+    uriUtil: '../node_modules/mongodb/index'
+  }
+});
