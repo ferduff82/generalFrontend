39e692e0af7dc8db6111ed37ea8b801f33aa9f71
Fernando Arias <fernando.arias@tradehelm.com>
2017-01-04 14:33:42 -0300

    New file deferred

---
+++ Experimentation/Learning resources/JS Promises & Jquery Deferred/Deferred.js
@@ -0,0 +1,38 @@
+
+function getCustomer(customerId){
+    var d = $.Deferred();
+    
+    $.post( "/echo/json/", {
+        json: JSON.stringify({firstName: "Jose", lastName: "Romaniello", ssn: "123456789"}),
+        delay: 4
+    }).done(function(p){
+        d.resolve(p);
+    }).fail(d.reject); 
+    return d.promise();
+}
+
+function getPersonAddressBySSN(ssn){
+    return $.post("/echo/json/", {
+         json: JSON.stringify({
+              ssn: "123456789",
+              address: "Siempre Viva 12345, Springfield" }),
+         delay: 2
+    }).pipe(function(p){
+        return p.address;
+    });
+}
+
+function load(){
+    $.blockUI({message: "Loading..."});
+
+    var loadingCustomer = getCustomer(123).done(function(c){
+        $("span#firstName").html(c.firstName)
+    });
+    var loadingAddress = getPersonAddressBySSN("123456789").done(function(address){
+        $("span#address").html(address)
+    });
+    
+    $.when(loadingCustomer, loadingAddress).done($.unblockUI);
+}
+
+load();
