966e7da861ad1b40609184d519d6eed645cbf1cf
Fernando Arias <fernando.arias@tradehelm.com>
2017-04-04 10:58:52 -0300

    New jquery data

---
+++ Experimentation/Learning resources/Javascript/Jquery Advanced/Should Use.js
@@ -0,0 +1,11 @@
+
+/* Jquery Usefull methods */
+
+change(); // Like angular for validation: 
+map();
+wrap();
+//https://code.tutsplus.com/tutorials/20-helpful-jquery-methods-you-should-be-using--net-10521
+
+$.blockUI();
+$.unblockUI();
+$.Deferred;
