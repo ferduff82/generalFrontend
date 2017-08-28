effd488eac1fc72e46390e8391514262244651df
Fernando Arias <ferduff82@hotmail.com>
2017-08-28 18:49:14 -0300

    bug fixing to added files

---
+++ Experimentation/Development/Daily-Questions/questions/questionsCSS.js
@@ -0,0 +1,9 @@
+var questionsCSS = [
+	"Que son las HTML entities?",
+	"Defina Modelo de Caja CSS",
+	"Explique el uso de box-sizing: border-box;",
+	"Explique el uso de table-layout: fixed;",
+	"Que es el & y que es el >?",
+	"Que es más pesado al aplicar en CSS, opacity o drop shadow y por que?",
+	"Que diferencia hay entre position:absolute y position:relative?"
+];
