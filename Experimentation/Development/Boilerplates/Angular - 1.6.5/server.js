65f2bed89145d15b83b673b99ab81ad0687a1c53
Fernando Arias <ferduff82@hotmail.com>
2017-09-16 14:26:43 -0300

    Added opn library into nodeJS server

--- Experimentation/Development/Boilerplates/Angular - 1.6.5/server.js
+++ Experimentation/Development/Boilerplates/Angular - 1.6.5/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var opn = require('opn');
 
 app.use(express.static(__dirname + '/public'));  
 
@@ -8,4 +9,7 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8000);
+app.listen(8000, 'localhost', function(){
+	opn('http://localhost:8000', {app: 'chrome'});
+	console.log('Server running at: localhost:8000');
+});
